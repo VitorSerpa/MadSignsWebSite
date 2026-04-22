8b3a59ebc87622067d1de985aed65426a2dc5b7c
Laís <zanardilais@gmail.com>
2026-04-22 08:54:01 -0300

    feat(Icon adiconado)

--- mad-signs-page/app/layout.tsx
+++ mad-signs-page/app/layout.tsx
@@ -2,6 +2,12 @@ import './globals.css';
 
 export const metadata = {
   viewport: "width=device-width, initial-scale=1",
+  title: 'Mad Signs Portas ACM',
+  icons: {
+    icon: '/images/favicon.ico',
+    shortcut: '/images/favicon.ico',
+    apple: '/apple-icon.png',
+  },
 };
 
 export default function RootLayout({
